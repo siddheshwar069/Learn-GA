4c1d1f701fb7d1498e0b129e7acb826b0419d909
Svaidya <siddheshwar069@gmail.com>
2025-09-09 23:12:38 +0530

    updated HelperBox.jsx

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,9 +1,9 @@
-import './HelpBox.css';
+import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
